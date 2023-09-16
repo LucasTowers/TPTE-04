db9ef3b38c2ffc406c5d9921d3e83f41fa866a2d
Lucas <lucasgtorres1@gmail.com>
2023-09-15 23:43:36 -0300

    ex02

---
+++ Atividade01Ex02/AvaliacaoDeAluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade01Ex02
+{
+    public class AvaliacaoDeAluno
+    {
+        private double nota;
+
+        public AvaliacaoDeAluno()
+        {
+            nota = 0; // Inicializa a nota com 0.
+        }
+
+        public double Nota
+        {
+            get { return nota; }
+            set
+            {
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentException("A nota deve estar entre 0 e 10.");
+                }
+                nota = value;
+            }
+        }
+    }
+}
---
+++ Atividade01Ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿using Atividade01Ex02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AvaliacaoDeAluno aluno = new AvaliacaoDeAluno();
+
+        try
+        {
+            Console.Write("Digite a nota do aluno: ");
+            double nota = Convert.ToDouble(Console.ReadLine());
+
+            aluno.Nota = nota;
+
+            Console.WriteLine($"A nota do aluno é: {aluno.Nota}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um número válido.");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
