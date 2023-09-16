7eeea2d0b3a64e33eaeac89f74e714b77e76e8fb
Lucas <lucasgtorres1@gmail.com>
2023-09-15 23:35:59 -0300

    ex01

---
+++ Atividade04Ex01/EstoqueDeProdutos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade04Ex01
+{
+    public class EstoqueDeProdutos
+    {
+        private int quantidadeProdutos;
+
+        public EstoqueDeProdutos()
+        {
+            quantidadeProdutos = 0; // Inicializa o estoque com 0 produtos.
+        }
+
+        public int QuantidadeProdutos
+        {
+            get { return quantidadeProdutos; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A quantidade de produtos não pode ser negativa.");
+                }
+                quantidadeProdutos = value;
+            }
+        }
+
+        // Outros métodos e propriedades relacionados ao estoque podem ser adicionados aqui.
+    }
+}
---
+++ Atividade04Ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿using Atividade04Ex01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EstoqueDeProdutos estoque = new EstoqueDeProdutos();
+
+        Console.WriteLine("Bem-vindo ao sistema de controle de estoque!");
+
+        while (true)
+        {
+            Console.WriteLine($"Quantidade atual de produtos em estoque: {estoque.QuantidadeProdutos}");
+
+            Console.Write("Digite a quantidade de produtos a ser adicionada (ou negativa para sair): ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int quantidade))
+            {
+                try
+                {
+                    estoque.QuantidadeProdutos += quantidade;
+                    Console.WriteLine("Quantidade atualizada com sucesso!");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
