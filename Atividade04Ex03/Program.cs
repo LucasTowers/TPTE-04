0c5a72f41d922431095497e60858a153e1ecfc47
Lucas <lucasgtorres1@gmail.com>
2023-09-18 23:41:44 -0300

    Atividade04Ex03

---
+++ Atividade04Ex03/Program.cs
@@ -0,0 +1,29 @@
+﻿using Atividade04Ex03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            ReservaDeVoo reserva = new ReservaDeVoo(100); // Define a capacidade inicial do avião.
+
+            Console.WriteLine($"Capacidade do avião: {reserva.Capacidade}");
+            Console.WriteLine($"Assentos reservados: {reserva.AssentosReservados}");
+
+            reserva.ReservarAssentos(30); // Reserva 30 assentos.
+            Console.WriteLine($"Assentos reservados: {reserva.AssentosReservados}");
+
+            reserva.ReservarAssentos(80); // Tenta reservar mais 80 assentos.
+            Console.WriteLine($"Assentos reservados: {reserva.AssentosReservados}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
---
+++ Atividade04Ex03/ReservaDeVoo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade04Ex03
+{
+    public class ReservaDeVoo
+    {
+        private int capacidade;
+        private int assentosReservados;
+
+        public ReservaDeVoo(int capacidadeInicial)
+        {
+            if (capacidadeInicial <= 0)
+            {
+                throw new ArgumentException("A capacidade deve ser maior que zero.");
+            }
+
+            capacidade = capacidadeInicial;
+            assentosReservados = 0; // Inicializa o número de assentos reservados com zero.
+        }
+
+        public int AssentosReservados
+        {
+            get { return assentosReservados; }
+        }
+
+        public int Capacidade
+        {
+            get { return capacidade; }
+        }
+
+        public void ReservarAssentos(int quantidade)
+        {
+            if (quantidade < 0)
+            {
+                throw new ArgumentException("A quantidade de assentos a serem reservados não pode ser negativa.");
+            }
+
+            if (assentosReservados + quantidade > capacidade)
+            {
+                throw new InvalidOperationException("A capacidade do avião foi excedida. Não é possível reservar mais assentos.");
+            }
+
+            assentosReservados += quantidade;
+        }
+
+        // Outros métodos e propriedades relacionados à reserva de voos podem ser adicionados aqui.
+    }
+}
