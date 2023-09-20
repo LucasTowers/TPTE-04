8066f4daef5246fde6e73514a43dbfd86a8716f7
Lucas <lucasgtorres1@gmail.com>
2023-09-19 23:31:16 -0300

    Atividade04Ex07

---
+++ Atividade04Ex07/ControleDeTemperatura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade04Ex07
+{
+    public class ControleDeTemperatura
+    {
+        private double temperaturaDefinida;
+
+        public double TemperaturaDefinida
+        {
+            get { return temperaturaDefinida; }
+            set
+            {
+                if (value < 16 || value > 30)
+                {
+                    throw new ArgumentException("A temperatura definida deve estar entre 16°C e 30°C.");
+                }
+                temperaturaDefinida = value;
+            }
+        }
+
+        public ControleDeTemperatura(double temperaturaInicial)
+        {
+            TemperaturaDefinida = temperaturaInicial;
+        }
+
+        // Outros métodos relacionados ao controle de temperatura podem ser adicionados aqui.
+    }
+}
---
+++ Atividade04Ex07/Program.cs
@@ -0,0 +1,23 @@
+﻿using Atividade04Ex07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            ControleDeTemperatura termostato = new ControleDeTemperatura(22);
+
+            Console.WriteLine($"Temperatura definida: {termostato.TemperaturaDefinida}");
+
+            termostato.TemperaturaDefinida = 25; // Define uma nova temperatura
+            Console.WriteLine($"Nova temperatura definida: {termostato.TemperaturaDefinida}");
+
+            termostato.TemperaturaDefinida = 10; // Tentando definir uma temperatura fora do intervalo permitido
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
