b671ae17af1540328e31c6b4fee8b27d4f798f4f
Lucas <lucasgtorres1@gmail.com>
2023-09-22 23:22:11 -0300

    Atividade04Ex08

---
+++ Atividade04Ex08/GerenciamentoDeEstacionamento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade04Ex08
+{
+    public class GerenciamentoDeEstacionamento
+    {
+        private int capacidade;
+        private int vagasDisponiveis;
+
+        public GerenciamentoDeEstacionamento(int capacidadeEstacionamento)
+        {
+            if (capacidadeEstacionamento <= 0)
+            {
+                throw new ArgumentException("A capacidade do estacionamento deve ser maior que zero.");
+            }
+
+            capacidade = capacidadeEstacionamento;
+            vagasDisponiveis = capacidadeEstacionamento; // Inicializa todas as vagas como disponíveis.
+        }
+
+        public void EstacionarCarro()
+        {
+            if (vagasDisponiveis == 0)
+            {
+                throw new InvalidOperationException("O estacionamento está lotado. Não é possível estacionar mais carros.");
+            }
+
+            vagasDisponiveis--;
+        }
+
+        public void SairDoEstacionamento()
+        {
+            if (vagasDisponiveis == capacidade)
+            {
+                throw new InvalidOperationException("O estacionamento já está vazio. Não é possível sair.");
+            }
+
+            vagasDisponiveis++;
+        }
+
+        public int VagasDisponiveis
+        {
+            get { return vagasDisponiveis; }
+        }
+    }
+}
---
+++ Atividade04Ex08/Program.cs
@@ -0,0 +1,33 @@
+﻿using Atividade04Ex08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            GerenciamentoDeEstacionamento estacionamento = new GerenciamentoDeEstacionamento(10); // Define a capacidade do estacionamento.
+
+            Console.WriteLine($"Vagas disponíveis: {estacionamento.VagasDisponiveis}");
+
+            estacionamento.EstacionarCarro();
+            Console.WriteLine($"Vagas disponíveis após estacionar um carro: {estacionamento.VagasDisponiveis}");
+
+            estacionamento.SairDoEstacionamento();
+            Console.WriteLine($"Vagas disponíveis após um carro sair: {estacionamento.VagasDisponiveis}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                estacionamento.EstacionarCarro(); // Tentando estacionar mais carros do que a capacidade
+            }
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
